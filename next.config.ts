0e9a5a8b1344f6d9d641ba798a2a33cdb3d97617
Danylo Haskov <haskoov@gmail.com>
2026-04-23 16:26:09 +0200

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-output: 'export',
+  output: "export",
+  basePath: isProd ? "/socialmedia" : "",
+  assetPrefix: isProd ? "/socialmedia/" : "",
   images: {
     unoptimized: true,
-  },};
+  },
+};
 
 export default nextConfig;
